f69ba1e37be0ec371704802f18cc0ed38d34cabd
Aleksandra Kovaleva <ak.nova8yte@gmail.com>
2025-03-04 20:40:50 +0500

    Add ignore for eslint per https://nextjs.org/docs/app/api-reference/config/eslint#disabling-rules

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  eslint: {
+    // Disable ESLint during production build to allow GitHub Pages deployment
+    ignoreDuringBuilds: true,
+  },
   basePath: process.env.GITHUB_PAGES ? '/undo-wiki' : '',
   images: { unoptimized: true },
   // Remove images from experimental section
